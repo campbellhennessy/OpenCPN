04b47d5b9e832623378106dbc13ddab023db67d5
campbellhennessy <campbell.hennessy@gmail.com>
2020-06-03 11:20:49 -0500

    Updated formatting of course over ground and heading values to include a leading zero and to show one decimal point

--- plugins/dashboard_pi/src/dashboard_pi.cpp
+++ plugins/dashboard_pi/src/dashboard_pi.cpp
@@ -3534,11 +3534,11 @@ void DashboardWindow::SetInstrumentList( wxArrayInt list )
                 break;
             case ID_DBP_I_COG:
                 instrument = new DashboardInstrument_Single( this, wxID_ANY,
-                        getInstrumentCaption( id ), OCPN_DBP_STC_COG, _T("%.0f") );
+                        getInstrumentCaption( id ), OCPN_DBP_STC_COG, _T("%05.1f") );
                 break;
             case ID_DBP_M_COG:
                 instrument = new DashboardInstrument_Single( this, wxID_ANY,
-                        getInstrumentCaption( id ), OCPN_DBP_STC_MCOG, _T("%.0f") );
+                        getInstrumentCaption( id ), OCPN_DBP_STC_MCOG, _T("%05.1f") );
                 break;
             case ID_DBP_D_COG:
                 instrument = new DashboardInstrument_Compass( this, wxID_ANY,
@@ -3567,11 +3567,11 @@ void DashboardWindow::SetInstrumentList( wxArrayInt list )
             case ID_DBP_I_HDT: //true heading
                 // TODO: Option True or Magnetic
                 instrument = new DashboardInstrument_Single( this, wxID_ANY,
-                        getInstrumentCaption( id ), OCPN_DBP_STC_HDT, _T("%.0f") );
+                        getInstrumentCaption( id ), OCPN_DBP_STC_HDT, _T("%05.1f") );
                 break;
             case ID_DBP_I_HDM:  //magnetic heading
                 instrument = new DashboardInstrument_Single( this, wxID_ANY,
-                        getInstrumentCaption( id ), OCPN_DBP_STC_HDM, _T("%.0f") );
+                        getInstrumentCaption( id ), OCPN_DBP_STC_HDM, _T("%05.1f") );
                 break;
             case ID_DBP_D_AW:
             case ID_DBP_D_AWA:
@@ -3932,4 +3932,3 @@ void OCPNFontButton::UpdateFont()
                                   m_selectedFont.GetPointSize()));
     }
 }
-
